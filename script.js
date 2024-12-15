c53eed391af7c940fa7e0bb887d1e2b02970adc6
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-15 20:40:39 +0100

    Add ref and push method
    to be able to push a lead to my Firebase realtime database

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+import { getDatabase,
+         ref,
+         push } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
 
 const firebaseConfig = {
     databaseURL: "https://leads-tracker-app-94ead-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -7,6 +9,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
+const referenceInDB = ref(database, "leads");
 
 // console.log(app)
 // console.log(database)
@@ -35,6 +38,6 @@ deleteBtn.addEventListener("dblclick", function() {
 })
 
 inputBtn.addEventListener("click", function() {
-    console.log(inputEl.value);
+    push(referenceInDB, inputEl.value);
     inputEl.value = "";
 });
