247daf1fd08ce52bf4a9ba75b8a3743f3939adbc
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-15 20:16:23 +0100

    Remove local storage
    Since I don't use the local storage anymore, but the realtime database of Firebase instead,I also delete all code associated to local storage

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ const database = getDatabase(app)
 // console.log(database)
 // console.log(firebaseConfig.databaseURL);
 
-localStorage.clear();
+// localStorage.clear();
 let myLeads = [];
 let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+// const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage;
-    render(myLeads);
-}
+// if (leadsFromLocalStorage) {
+//     myLeads = leadsFromLocalStorage;
+//     render(myLeads);
+// }
 
 function render(leads) {
     let listItems = "";
@@ -39,7 +39,7 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear();
+    // localStorage.clear();
     myLeads = [];
     render(myLeads);
 })
@@ -47,6 +47,6 @@ deleteBtn.addEventListener("dblclick", function() {
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    // localStorage.setItem("myLeads", JSON.stringify(myLeads));
     render(myLeads);
 });
