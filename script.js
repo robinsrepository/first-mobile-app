53150578acd9a66f20af86c6461906e58ff40d5d
RobinvanKempen <149374103+robinsrepository@users.noreply.github.com>
2024-12-15 20:19:07 +0100

    Delete code associated to myLeads array
    I also don't need to store anything into the myLeads array anymore, because I'm goint to store everythin in the realtime Firebase database instead

--- script.js
+++ script.js
@@ -12,18 +12,10 @@ const database = getDatabase(app)
 // console.log(database)
 // console.log(firebaseConfig.databaseURL);
 
-// localStorage.clear();
-let myLeads = [];
 let inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
-// const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
-// if (leadsFromLocalStorage) {
-//     myLeads = leadsFromLocalStorage;
-//     render(myLeads);
-// }
 
 function render(leads) {
     let listItems = "";
@@ -39,14 +31,10 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener("dblclick", function() {
-    // localStorage.clear();
-    myLeads = [];
-    render(myLeads);
+
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
+    console.log(inputEl.value);
     inputEl.value = "";
-    // localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    render(myLeads);
 });
